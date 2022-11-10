f508c168ffb88440fc95ea66fa82bd09f9f94958
Arevalinchy <75390468+Arevalinchy@users.noreply.github.com>
2022-11-10 17:21:15 -0500

    Update main.dart

--- lib/api-client/main.dart
+++ lib/api-client/main.dart
@@ -34,10 +34,14 @@ class _MyHomePageState extends State<MyHomePage> {
   late Future<AuthCheck> _checkAuthbyEmail;
 
   late Future<AV> _ambientVariables;
+  late Future<AV> _ambientVariables2;
+
   late Future<AVI> _ambVarInterval;
+  late Future<AVI> _ambVarInterval2;
   late Future<String> _writeAVI;
 
   late Future<AC> _actuatorConfig;
+  late Future<AC> _actuatorConfig2;
   late Future<String> _writeAC;
 
   late Future<String> _newUser;
@@ -52,13 +56,13 @@ class _MyHomePageState extends State<MyHomePage> {
     /*_ambientVariables = api.getAV(
         'dave',
         '46399ec9bb61442d6988d5daaf58e16cf58c4e03dfa75b1a00b99e567446293689ce24f6db220f02932561a408e8b15e29e06d6cb2b44c8032619a655da7ede8',
-        'sex',
+        'temperature',
         '2022/12/12 10:00:00',
         '2022/12/24 10:00:01');*/
     /*_ambVarInterval = api.getAmVarInt(
         'dave',
         '46399ec9bb61442d6988d5daaf58e16cf58c4e03dfa75b1a00b99e567446293689ce24f6db220f02932561a408e8b15e29e06d6cb2b44c8032619a655da7ede8',
-        'humidity');*/
+        'temperature');*/
     /*_writeAVI = api.writeAmbVarInterval(
         'dave',
         '46399ec9bb61442d6988d5daaf58e16cf58c4e03dfa75b1a00b99e567446293689ce24f6db220f02932561a408e8b15e29e06d6cb2b44c8032619a655da7ede8',
@@ -77,10 +81,10 @@ class _MyHomePageState extends State<MyHomePage> {
         "efabe459cbba64347969017bb8b8acaa2c75e66fd9e8e3700d91628fd0934c7130bc49cc35d340e601c10c7818d3a0c1c419cf5d8fcb18edbd8680e6d9b76b75",
         "esp32/dave/00:00:00:00:00:69/temperature");*/
 
-    _actuatorConfig = api.getActConfig(
+    /*_actuatorConfig = api.getActConfig(
         "dave",
         "46399ec9bb61442d6988d5daaf58e16cf58c4e03dfa75b1a00b99e567446293689ce24f6db220f02932561a408e8b15e29e06d6cb2b44c8032619a655da7ede8",
-        "water pump");
+        "water pump");*/
 
     /* _writeAC = api.writeActuatorsConfig(
         "dave",
@@ -90,6 +94,15 @@ class _MyHomePageState extends State<MyHomePage> {
         "10:00:01",
         0,
         1);*/
+    _ambientVariables2 = api.getAV2("dave",
+        "46399ec9bb61442d6988d5daaf58e16cf58c4e03dfa75b1a00b99e567446293689ce24f6db220f02932561a408e8b15e29e06d6cb2b44c8032619a655da7ede8");
+
+    /*_ambVarInterval2 = api.getAmVarInt2(
+      "dave",
+      "46399ec9bb61442d6988d5daaf58e16cf58c4e03dfa75b1a00b99e567446293689ce24f6db220f02932561a408e8b15e29e06d6cb2b44c8032619a655da7ede8",
+    );*/
+    /*_actuatorConfig2 = api.getActConfig2("dave",
+        "46399ec9bb61442d6988d5daaf58e16cf58c4e03dfa75b1a00b99e567446293689ce24f6db220f02932561a408e8b15e29e06d6cb2b44c8032619a655da7ede8");*/
   }
 
   @override
@@ -99,11 +112,11 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: FutureBuilder<AC>(
-            future: _actuatorConfig,
+        child: FutureBuilder<AV>(
+            future: _ambientVariables2,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
-                return Text(snapshot.data!.start_time['0']);
+                return Text("${snapshot.data!.varname['2']}");
               } else if (snapshot.hasError) {
                 return Text('${snapshot.error}');
               }
@@ -113,3 +126,8 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+
+
+//var = {0:tem, 1:hum, 2:lol, 3:tem}
+//min_ac = {0:a, 1:b, 2:c, 3:d}
