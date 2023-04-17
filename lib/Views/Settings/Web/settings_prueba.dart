e1310804b74e421f4046859edfa0be48678eb957
alejandrarchbold <alejandra.campo@urosario.edu.co>
2023-04-17 13:19:43 -0500

    cambios

--- lib/Views/Settings/Web/settings_prueba.dart
+++ lib/Views/Settings/Web/settings_prueba.dart
@@ -4,6 +4,11 @@ import 'package:responsivedashboard/constants.dart';
 import 'package:responsivedashboard/Widgets/menu.dart';
 import 'package:responsivedashboard/Widgets/dropdown.dart';
 
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+
+
 class SettingsDesktop2 extends StatefulWidget {
   const SettingsDesktop2({Key? key}) : super(key: key);
 
@@ -12,6 +17,26 @@ class SettingsDesktop2 extends StatefulWidget {
 }
 
 class _SettingsDesktop2State extends State<SettingsDesktop2> {
+
+  final _formKey = GlobalKey<FormState>();
+  String _forumText = "";
+  
+  
+  DateTime selectedDate = DateTime.now();
+
+  Future<DateTime> _selectDate(BuildContext context) async {
+  final DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2015, 8),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  } 
   @override
     Widget build(BuildContext context) {
     return Scaffold(
@@ -23,11 +48,7 @@ class _SettingsDesktop2State extends State<SettingsDesktop2> {
             padding: const EdgeInsets.all(30.0),
             child: Column(
               children: <Widget>[
-                Text('Select the frecuency you want the data to be displayed'),
-                Text(''),
-                DropdownButtonExample(),
-                Text('---prueba---'),
-                Text('------------'),
+                Text(''),               
                 ElevatedButton(
                       style:   ElevatedButton.styleFrom(
                         primary: Colors.green,
@@ -35,19 +56,49 @@ class _SettingsDesktop2State extends State<SettingsDesktop2> {
                             color: Colors.white, fontSize: 15, fontStyle: FontStyle.normal),
                         
                       ),
-                      onPressed: () => {print("ola")},
-                      child: Text('ADD PERSON'),
-                ),
-                Text('------------'),
+                      onPressed:() {
+                          // Show the registration form
+                          showRegistrationForm(context);
+                          },
+                      child: Text('CREATE ORGANIZATION'),         
+                    ),
+                
+                Text(''),
                 ElevatedButton(
                       style:   ElevatedButton.styleFrom(
                         primary: Colors.green,
                         textStyle: const TextStyle(
                             color: Colors.white, fontSize: 15, fontStyle: FontStyle.normal),
                       ),
-                      onPressed: () => {print("adios")},
+                      onPressed: () {
+                        showAddPersonForm(context);
+                        },
+                      child: Text('ADD PERSON'),
+                ),
+                ElevatedButton(
+                      style:   ElevatedButton.styleFrom(
+                        primary: Colors.green,
+                        textStyle: const TextStyle(
+                            color: Colors.white, fontSize: 15, fontStyle: FontStyle.normal),
+                      ),
+                      onPressed: () => {
+                        print("CONFIGURE INTERVALS")
+                        },
+                      child: Text('CONFIGURE INTERVALS'),
+                ),
+                Text(''),
+                ElevatedButton(
+                      style:   ElevatedButton.styleFrom(
+                        primary: Colors.green,
+                        textStyle: const TextStyle(
+                            color: Colors.white, fontSize: 15, fontStyle: FontStyle.normal),
+                      ),
+                      onPressed: ()  {
+                        showRemainderSetUP(context);
+                      },
                       child: Text('REMINDERS'),
                 ),
+                Text(''),
 
               ],
             ),
@@ -56,4 +107,126 @@ class _SettingsDesktop2State extends State<SettingsDesktop2> {
       
     );
   }
+  void showRegistrationForm(BuildContext context) {
+    // Create the registration form
+    Widget registrationForm = Container(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Name',
+            ),
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Description',
+            ),
+            keyboardType: TextInputType.emailAddress,
+          ),
+          ElevatedButton(
+            child: Text('Create'),
+            onPressed: () {
+              // Process the registration data
+              // (you can add your own logic here)
+              // ...
+
+              // Close the dialog
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+
+    // Show the registration form in a dialog box
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Create Organization'),
+          content: registrationForm,
+        );
+      },
+    );
+  }
+
+  void showAddPersonForm(BuildContext context){
+    Widget addPersonForm = Container (
+      padding: EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'E-mail'
+            ),
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Rol'
+            ),
+          ),
+          DropdownButtonExample(),
+          ElevatedButton(
+            child: Text('Add'),
+            onPressed: () {
+              Navigator.pop(context);
+            }, 
+          ),
+        ],
+      ),
+    );
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Add Person'),
+          content: addPersonForm,
+        );
+      },
+    ); 
+  }
+
+  void showRemainderSetUP(BuildContext context){
+    Widget reminderSetup= Container (
+      padding: EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              await _selectDate(context);
+            },
+            child: Text(
+              'Select Date',
+              style: TextStyle(
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          ElevatedButton(
+            child: Text('Add'),
+            onPressed: () {
+              Navigator.pop(context);
+            }, 
+          ),
+          Text('Selected date: ${DateFormat('yyyy-MM-dd').format(selectedDate)}',),
+        ],
+      ),
+    );
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Set-Up Reminder'),
+          content: reminderSetup,
+        );
+      },
+    ); 
+  }
+
 }
+
